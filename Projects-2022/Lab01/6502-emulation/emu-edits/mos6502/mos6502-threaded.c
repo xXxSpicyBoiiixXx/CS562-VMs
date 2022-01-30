c2b3aa6d12c601675b9510afd14a712dd11a79d6
Md Ali <mali54@hawk.iit.edu>
2022-01-30 10:17:26 -0600

    threaded code

---
+++ Projects-2022/Lab01/6502-emulation/emu-edits/mos6502/mos6502-threaded.c
@@ -0,0 +1,109 @@
+#include <rc.h>
+#include <base.h>
+#include <membus.h>
+#include <timekeeper.h>
+#include <mos6502/vmcall.h>
+#include <mos6502/mos6502.h>
+
+#include <string.h>
+
+void (*op_table[16][16])(mos6502_t *cpu) { 
+	{handle_BRK, handle_ORA_IDX_IDR, handle_JAM, handle_SLO_IDX_IDR, handle_NOP_ZP, handle_ORA_ZP, handle_ASL_ZP, handle_SLO_ZP, handle_PHP, handle_ORA, handle_ASL, handle_ANC, handle_NOP_ABS, handle_ORA_ABS, handle_ASL_ABS, handle_SLO_ABS}
+	{}
+	{}
+	{}
+	{}
+	{}
+	{}
+	{}
+	{}
+	{}
+	{}
+	{}
+	{}
+	{}
+	{}
+	{}
+	
+};
+
+static const uint8_t instr_cycles[256] = {
+	7, 6, 2, 8, 3, 3, 5, 5, 3, 2, 2, 2, 4, 4, 6, 6,
+	2, 5, 2, 8, 4, 4, 6, 6, 2, 4, 2, 7, 4, 4, 7, 7,
+	6, 6, 2, 8, 3, 3, 5, 5, 4, 2, 2, 2, 4, 4, 6, 6,
+	2, 5, 2, 8, 4, 4, 6, 6, 2, 4, 2, 7, 4, 4, 7, 7,
+	6, 6, 2, 8, 3, 3, 5, 5, 3, 2, 2, 2, 3, 4, 6, 6,
+	2, 5, 2, 8, 4, 4, 6, 6, 2, 4, 2, 7, 4, 4, 7, 7,
+	6, 6, 2, 8, 3, 3, 5, 5, 4, 2, 2, 2, 5, 4, 6, 6,
+	2, 5, 2, 8, 4, 4, 6, 6, 2, 4, 2, 7, 4, 4, 7, 7,
+	6, 6, 2, 6, 3, 3, 3, 3, 2, 2, 2, 2, 4, 4, 4, 4,
+	2, 6, 2, 6, 4, 4, 4, 4, 2, 5, 2, 5, 5, 5, 5, 5,
+	2, 6, 2, 6, 3, 3, 3, 3, 2, 2, 2, 2, 4, 4, 4, 4,
+	2, 5, 2, 5, 4, 4, 4, 4, 2, 4, 2, 4, 4, 4, 4, 4,
+	2, 6, 2, 8, 3, 3, 5, 5, 2, 2, 2, 2, 4, 4, 6, 6,
+	2, 5, 2, 8, 4, 4, 6, 6, 2, 4, 2, 7, 4, 4, 7, 7,
+	2, 6, 2, 8, 3, 3, 5, 5, 2, 2, 2, 2, 4, 4, 6, 6,
+	2, 5, 2, 8, 4, 4, 6, 6, 2, 4, 2, 7, 4, 4, 7, 7,
+};
+
+static inline uint8_t
+read8 (mos6502_t * cpu, uint16_t addr)
+{
+	return membus_read(cpu->bus, addr);
+}
+
+static inline void
+write8 (mos6502_t * cpu, uint16_t addr, uint8_t val)
+{
+	membus_write(cpu->bus, addr, val);
+}
+
+static inline uint16_t
+read16 (mos6502_t * cpu, uint16_t addr)
+{
+	uint16_t lo = (uint16_t)read8(cpu, addr);
+	uint16_t hi = (uint16_t)read8(cpu, addr + 1);
+	uint16_t val = lo | (uint16_t)(hi << 8);
+	return val;
+}
+
+static inline uint16_t
+buggy_read16 (mos6502_t * cpu, uint16_t addr)
+{
+	uint16_t first = addr;
+    uint16_t msb = addr & 0xff00;
+    uint16_t lsb = ((addr & 0xff) == 0xff) ? 0 : ((addr & 0xff) + 1);
+	uint16_t secnd = msb | lsb;
+	uint16_t lo = (uint16_t)read8(cpu, first);
+	uint16_t hi = (uint16_t)read8(cpu, secnd);
+	uint16_t val = (uint16_t)(hi << 8) | lo;
+	return val;
+}
+
+
+
+size_t
+mos6502_instr_repr (mos6502_t * cpu, uint16_t addr, char * buffer, size_t buflen)
+{
+	// FILL ME IN
+	// Fill in chart here 
+	// Delete this line when you're done
+	//buffer[0] = 0;
+	
+	uint8_t opcode = read8(cpu, addr); 
+	uint8_t hi = opcode >> 4; 
+	uint8_t lo = opcode & 0x0F; 
+	
+	return 0;
+}
+
+mos6502_step_result_t
+mos6502_step (mos6502_t * cpu)
+{
+	uint8_t opcode = read8(cpu, cpu->pc);
+
+	// FILL ME IN
+
+	mos6502_advance_clk(cpu, instr_cycles[opcode]);
+	return MOS6502_STEP_RESULT_SUCCESS;
+}
